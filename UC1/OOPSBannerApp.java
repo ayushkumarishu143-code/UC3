e22e064f9a0917127b6d7df1c522a1a3c6254727
ayushkumarishu143 <ayushkumar.ishu143@gmail.com>
2026-02-17 09:53:29 +0530

    update

---
+++ UC1/OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPS Banner Application
+ *
+ * UC1: Print literal text "OOPS" to the console
+ *
+ * @author Ishu
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+    }
+}
